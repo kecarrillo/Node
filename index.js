06d0bb433fde629bbba318c3ea0727815dacc356
Pierre-Jean <pierre-jean.sinacouty@imie.fr>
2020-07-07 23:41:13 +0200

    add bootstrap, jquery components, and configuration of the app

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const path = require('path');
 let app = express();
 
 app.set('appName', 'PhotoStream');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views') );
 app.set('view engine', 'pug');
+
+// Set Views Folder
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.all('*',(req, resp)=>{
     resp.render('index', { title: "PhotoStream",/*photos*/});
