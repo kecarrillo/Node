495941eff14c8ca572a1094b3cc31ffc79138052
greyr <kevin.carrillo@imie.fr>
2020-07-07 17:31:11 +0200

    bootstrap + pugs + css + statics

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+let app = express();
+
+app.set('appName', 'PhotoStream');
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views') );
+app.set('view engine', 'pug');
+
+app.all('*',(req, resp)=>{
+    resp.render('index', { title: "PhotoStream",/*photos*/});
+});
+
+http .createServer(app)
+    .listen(
+        app.get('port'),
+        () => {
+    console.log(`Express.js server ecoutes sur le port ${app.get('port')}`);
+} );
