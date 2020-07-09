f30b3fae5b04d59f33ca231201ebfd719feb54b2
greyr <kevin.carrillo@imie.fr>
2020-07-09 10:24:47 +0200

    Ajout de composants nécessaires à bootstrap

--- index.js
+++ index.js
@@ -31,8 +31,10 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.json());
 
 app.all('*',(req, resp)=>{
-    resp.render('index2', { title: "PhotoStream", datas: [{id: "1", title: "monTitre", author: "Bibi", body: ["monTitre2Tof", "/statics/img/ground.jpg"]},
-            {id: "2", title: "monTitre2", author: "Bibi", body: ["monTitre2Tof2", "/statics/img/exercice.jpg"]}]/*photos*/});
+    resp.render('index2',
+        { title: "PhotoStream", datas: [{id: "1", title: "monTitre", author: "Bibi", body: ["monTitre2Tof", "/statics/img/ground.jpg"]},
+        {id: "2", title: "monTitre2", author: "Bibi", body: ["monTitre2Tof2", "/statics/img/exercice.jpg"]}]
+        });
 });
 
 
